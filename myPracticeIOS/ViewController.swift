4eda812a645f2cfa6f913c72f8e480825a9eac39
jdaipopo <jdaipopo11@gmail.com>
2022-10-18 11:33:26 +0700

    fitst commit

---
+++ myPracticeIOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  myPracticeIOS
+//
+//  Created by สรวิชญ์ นิตรัตน์ on 12/10/2565 BE.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
